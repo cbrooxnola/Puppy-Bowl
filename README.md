b0bc5bba639638a8ad671ea1509b44c791bf79bb
Christopher <csbroox@gmail.com>
2023-08-31 10:00:35 -0500

    added readme and initial game plan

---
+++ README.md
@@ -0,0 +1,3 @@
+#Building an SPA that will display a list of items from an API,
+##and the details of any selected item,
+##(in theory)
---
+++ app.js
@@ -0,0 +1,9 @@
+//build initial html file
+//assign variables to html elements
+//source API
+//configure html navigation
+//make html adjustments as needed
+//make sure script is updated and correlated to html
+//make sure API data is sourced correctly and displayed to reflect such
+//give it some style
+
